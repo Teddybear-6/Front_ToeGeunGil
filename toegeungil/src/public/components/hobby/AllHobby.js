83820c639d6777c669456f9189e85af3870dd381
JIHWAN <jjjh0508@gmail.com>
2023-09-11 17:45:44 +0900

    작업이동

--- toegeungil/src/public/components/hobby/AllHobby.js
+++ toegeungil/src/public/components/hobby/AllHobby.js
@@ -3,14 +3,13 @@ import HobbyMain from './hobbyMain';
 import AllHobbyCss from './AllHobby.module.css'
 function AllHobby(){
     const [hobby, setHobby] = useState([]);
-
+    const [hobbySize , setHobbySize] =useState(0);
     useEffect(()=>{
-        fetch("http://localhost:8001/hobbys").then((response)=> response.json()).then((data)=> 
+        fetch(`http://localhost:8001/hobbys?page=${hobbySize}&size=12`).then((response)=> response.json()).then((data)=> 
             setHobby(data))
-    
+           
     },[])
     
-
     return(
         <>
             <div className={AllHobbyCss.main}>
