215c065ef807ad021f75b0d21dc5d1cb8a09622d
JIHWAN <jjjh0508@gmail.com>
2023-09-11 14:20:14 +0900

    카테고리 이름

--- toegeungil/src/public/components/HobbyKeyword.js
+++ toegeungil/src/public/components/HobbyKeyword.js
@@ -5,13 +5,11 @@ function HobbyKeyword({keyword}){
     const [keywordName, setKeywordName] = useState("");
 
     useEffect(()=>{
-        console.log(keyword)
+       
         fetch(`http://localhost:8001/keyword/${keyword}`).then(res=> res.json()).then(res=>setKeywordName(res));
     },[])
     return(
-        <>
         <p className={CardStyle.keywordName}>{keywordName.keywordName}</p>
-        </>
     )
 }
 
--- toegeungil/src/public/components/hobbyCard.js
+++ toegeungil/src/public/components/hobbyCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import HobbyKeyword from './HobbyKeyword';
 function HobbyCard(hobbys){
     const[mainImage, setMainImage] = useState();
+    const[category, setCategery] = useState([]);
 
     useEffect(()=>{
         fetch(`http://localhost:8001/hobbys/mainimages/${hobbys.hobbys.hobbyCode}`).then(res => res.blob())
@@ -13,6 +14,11 @@ function HobbyCard(hobbys){
             setMainImage(objectURL)
         }
         )
+
+        fetch(`http://localhost:8001/category/${hobbys.hobbys.categoryCode}`).then(res=>res.json()).then(data=>{
+            setCategery(data)
+           
+        })
     },[])
     
     return(
@@ -20,13 +26,22 @@ function HobbyCard(hobbys){
         <div className={CardStyle.hobbyCard}>
             <img className={CardStyle.hobbyImage} src={mainImage}></img>
             <p className={CardStyle.hobbyTitle}>{hobbys.hobbys.hobbyTitle}</p>
-            <div>
-            {hobbys.hobbys.keyword && hobbys.hobbys.keyword.map((keyword,index)=>(
-               <div className={CardStyle.keywordCard}>
-                <HobbyKeyword keyword={keyword.keywordCode} key={index}/>
-                </div> 
-            ))}
-            </div>
+           
+           <div>
+                <div className={CardStyle.keywordCard}>
+                <p className={CardStyle.keywordName}>{category.categoryName}</p>
+                </div>
+                {hobbys.hobbys.keyword && hobbys.hobbys.keyword.map((keyword,index)=>(
+                    <div className={CardStyle.keywordCard}>
+                        <HobbyKeyword keyword={keyword.keywordCode} key={index}/>
+                    </div>
+                    
+             ))}
+
+                <div className={CardStyle.keywordUnName}>
+               <p className={CardStyle.keywordName}>···</p>
+               </div>
+               </div>
             <p className={CardStyle.hobbyPrice}> {hobbys.hobbys.hobbyPrice}원</p>
         </div>
         </>
