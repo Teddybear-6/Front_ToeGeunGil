705e524faf0cc86aa61649846230f14a1328b150
bearnyong <rhalsdud216@gmail.com>
2023-09-11 14:16:43 +0900

    social_Main page 생성

--- toegeungil/src/App.js
+++ toegeungil/src/App.js
@@ -1,11 +1,12 @@
-import MainRouter from "./router/MainRouter";
+// import MainRouter from "./router/MainRouter";
+import SocialMain from "./public/social/page/SocialMain";
 
 function App() {
   return (
     <>
-   <h1>프로젝트 퇴근길</h1>
-    <MainRouter/>
-    
+      <h1>프로젝트 퇴근길</h1>
+      {/* <MainRouter/> */}
+      <SocialMain/>
     </>
   );
 }
---
+++ toegeungil/src/public/social/page/SocialMain.js
@@ -0,0 +1,8 @@
+
+function SocialMain() {
+    return(
+        <h1>소셜 메인 화면</h1>
+    )
+}
+
+export default SocialMain;
