8cc94e7eee5eabbd7c6faea7e3056517edb5be4d
afdsj <thdla3441@gmail.com>
2023-09-12 16:37:32 +0900

    NoticeTableColumn 생성

---
+++ toegeungil/src/public/notice/components/NoticeTableColumn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoticeTableColumn = ({ children }) => {
+    return (
+        <td className="notice-table-column">
+            {
+                children
+            }
+        </td>
+    )
+}
+
+export default NoticeTableColumn;
