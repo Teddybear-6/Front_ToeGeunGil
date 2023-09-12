a29fe35d15090be626f9c894d0ad2ecb0742a0e0
bearnyong <rhalsdud216@gmail.com>
2023-09-12 10:29:26 +0900

    @CrossOrigin port=3000 연결

--- toegeungil/src/App.js
+++ toegeungil/src/App.js
@@ -1,5 +1,6 @@
 // import MainRouter from "./router/MainRouter";
 import SocialMain from "./public/social/page/SocialMain";
+import AllSocial from "./public/social/api/AllSocial";
 
 function App() {
   return (
@@ -7,9 +8,9 @@ function App() {
       <h1>프로젝트 퇴근길</h1>
       {/* <MainRouter/> */}
       <SocialMain/>
+      <AllSocial/>
     </>
   );
 }
 
 export default App;
-
--- toegeungil/src/public/social/api/AllSocial.js
+++ toegeungil/src/public/social/api/AllSocial.js
@@ -5,15 +5,16 @@ import SocialMain from "../page/SocialMain";
 function AllSocial() {
     const [social, setSocial] = useState([]);
 
+    /*get 호출 -> fetch() 함수는 디폴트로 GET방식으로 작동*/
     useEffect(()=> {
-        fetch("http://localhost:8001/socials").then((response) => response.json()).then((date) => setSocial(date))
-    }, [])
+        fetch("http://localhost:8001/socials/3").then((response) => response.json()).then((date) => setSocial(date))
+    }, []);
 
     return(
-        <>
-            <div className="">
-                <SocialMain socials={social}/>
-            </div>
-        </>
+        <div className="">
+            {social}
+        </div>
     )
 }
+
+export default AllSocial;
