2976f5d256c929dddc5770ca107a7b2257e00a05
afdsj <thdla3441@gmail.com>
2023-09-13 15:45:59 +0900

    NoticeList 수정

--- toegeungil/src/public/pages/NoticeList.js
+++ toegeungil/src/public/pages/NoticeList.js
@@ -1,19 +1,14 @@
-import { Children, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import NoticeTable from "../components/NoticeTable";
-import NoticeTableRow from "../components/NoticeTableRow";
-import NoticeTableColumn from "../components/NoticeTableColumn";
 
 const NoticeList = () => {
     const [notice, setNotice] = useState([]);
-    const [error, setError] = useState(null);
 
     useEffect(
         () => {
             fetch("http://localhost:8001/notices")
                 .then(response => response.json())
-                // .then((data) => console.log(data))
                 .then(data => setNotice(data))
-                .catch((error) => setError(error))
         }, [])
 
     return (
