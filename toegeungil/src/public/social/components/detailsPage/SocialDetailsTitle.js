e07ea887f92a1ae41e496f741f6837dd57fc79f2
bearnyong <rhalsdud216@gmail.com>
2023-09-14 20:29:02 +0900

    socialDetailsSchedule

--- toegeungil/src/public/social/components/detailsPage/SocialDetailsTitle.js
+++ toegeungil/src/public/social/components/detailsPage/SocialDetailsTitle.js
@@ -34,43 +34,61 @@ function SocialDetailsTitle() {
 
     return (
         <>
-            <div>
-                {/* 소셜 제목, 키워드, 작성자 */}
-                <div className={DetailsStyle.socialDetailsTitleBoard}>
-                    {/* 소셜 제목 */}
-                    <div className={DetailsStyle.socialDetailsTitle}>{socials.socialName}</div>
-                    <div className={DetailsStyle.flexStyleKeywordAndUser}>
-                        {/* 키워드 */}
-                        <div className={DetailsStyle.flexStyle}>
-                            {socials.keywordDTOList?.map((m, index) => {
-                                return (
-                                    <SocialKeyword key={index} code={m} />
-                                )
-                            })}
+            <div className={DetailsStyle.flexStyle}>
+                <div>
+                    {/* 소셜 제목, 키워드, 작성자 */}
+                    <div className={DetailsStyle.socialDetailsTitleBoard}>
+                        {/* 소셜 제목 */}
+                        <div className={DetailsStyle.socialDetailsTitle}>{socials.socialName}</div>
+                        <div className={DetailsStyle.flexStyleKeywordAndUser}>
+                            {/* 키워드 */}
+                            <div className={DetailsStyle.flexStyle}>
+                                {socials.keywordDTOList?.map((m, index) => {
+                                    return (
+                                        <SocialKeyword key={index} code={m} />
+                                    )
+                                })}
+                            </div>
+                            {/* 작성자 */}
+                            <div className={DetailsStyle.socialDetailsWriter}>
+                                <label>작성자 : </label>
+                                <UserNicname userNo={socials.userNum} />
+                            </div>
                         </div>
-                        {/* 작성자 */}
-                        <div className={DetailsStyle.socialDetailsWriter}>
-                            <label>작성자 : </label>
-                            <UserNicname userNo={socials.userNum} />
+                    </div>
+                    {/* 사진 */}
+                    <SocialDetailsImage imgcode={socials.fileNum} />
+                </div>
+                <div className={DetailsStyle.socialDetailsScheduleBoard}>
+                    <div className={DetailsStyle.socialDetailsScheduleLayout}>
+                        <div className={DetailsStyle.socialDetailsScheduleTitle}>일정</div>
+                        {/* 모임일시 */}
+                        <div className={DetailsStyle.socialDetailsScheduleContent}>
+                            일시 : {socials.socialDate}
+                        </div>
+                        {/* 장소(지역, 상세주소) */}
+                        <div className={DetailsStyle.socialDetailsScheduleContent}>
+                            <div className={DetailsStyle.flexStyle}>
+                            장소 :&nbsp;
+                            <SocialLocal code={socials.localCode}/>&nbsp;
+                            {socials.localDetails}
+                            </div>
+                        </div>
+                        {/* 시간(시작시간, ~종료시간) */}
+                        <div className={DetailsStyle.socialDetailsScheduleContent}>
+                            시간 :&nbsp;
+                            {socials.socialStartTime}
+                            <label>~</label>
+                            {socials.socialEndTime}
+                        </div>
+                        {/* 정원(명) */}
+                        <div className={DetailsStyle.socialDetailsScheduleContent}>
+                            인원 :&nbsp;
+                            {socials.socialFixedNum}
+                            <label>명</label>
                         </div>
                     </div>
                 </div>
-                {/* 사진 */}
-                <SocialDetailsImage imgcode={socials.fileNum} />
-            </div>
-            <div>
-                {/* 모임일시 */}
-                <div>{socials.socialDate}</div>
-                {/* 장소(지역, 상세주소) */}
-                <div>
-                    <label><SocialLocal code={socials.localCode}/></label>
-                    <label>{socials.localDetails}</label>
-                </div>
-                {/* 시간(시작시간, ~종료시간) */}
-                <div>{socials.socialStartTime}</div>
-                <div>{socials.socialEndTime}</div>
-                {/* 정원(명) */}
-                <div>{socials.socialFixedNum}<label>명</label></div>
             </div>
         </>
     )
