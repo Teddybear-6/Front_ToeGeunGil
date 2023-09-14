b15dcaace23ff464d5ecfb9ee2b1ac7a965f49a6
afdsj <thdla3441@gmail.com>
2023-09-14 18:18:44 +0900

    NoticeView_상세페이지

--- toegeungil/src/App.js
+++ toegeungil/src/App.js
@@ -1,13 +1,27 @@
 // import MainRouter from "./router/MainRouter";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoticeMain from "./public/pages/NoticeMain";
+import NoticeView from './public/pages/NoticeView';
 
 function App() {
   return (
     <>
       <h1>프로젝트 퇴근길</h1>
+      <NoticeView/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='notice'>
+          <Route index element={<NoticeMain/>}/>
+          <Route path=':noticeNum' element={<NoticeView/>}/>
+        </Route>
+      </Routes>
+
+      </BrowserRouter>
+            {/* <Route exact path='/noticeView/:number' Component={NoticeView}/>
+      <Route exact path='/' Component={NoticeMain}/> */}
       {/* <MainRouter/> */}
-      <NoticeMain/>
+      {/* <NoticeMain/>
+      <NoticeView/> */}
     </>
   );
 }
--- toegeungil/src/public/pages/NoticeView.js
+++ toegeungil/src/public/pages/NoticeView.js
@@ -1,10 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import '../components/NoticeView.css';
+import detailCss from '../components/NoticeView.css';
 
-const NoticeView = props => {
-    return(
-        <>
-        
-        </>
+
+const NoticeView = ({history}) => {
+    const { noticeNum } = useParams();
+    const [detail, setDetail] = useState({});
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8001/notices/${noticeNum}`)
+                .then(response => response.json())
+                .then(data => setDetail(data))
+        }, [])
+
+    return (
+        <div className="view-wrapper">
+            {
+                detail? (
+                    <>
+                        <div className="view-name">
+                            <label>{detail.noticeTitle}</label>
+                        </div>
+                        <div className="view-date">
+                            <label>{detail.noticeDate}</label>
+                        </div>
+                        <div className="view-content">
+                            <label>{detail.noticeContent}</label>
+                        </div>
+                    </>
+                ) : "공지사항이 없습니다"
+            }
+            <button className="view-button" onClick={(history)}>목록</button>
+        </div>
     )
 }
 
