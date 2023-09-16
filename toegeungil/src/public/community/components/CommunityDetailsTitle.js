9e87c049435664e08a4eb4f2a542347bef47a2da
dleksql1227 <dleksql1227@gmail.com>
2023-09-17 08:45:36 +0900

    LocationNum 데이터 초기화 설정

--- toegeungil/src/public/community/components/CommunityDetailsTitle.js
+++ toegeungil/src/public/community/components/CommunityDetailsTitle.js
@@ -2,20 +2,24 @@ import { useEffect, useState } from "react";
 import DetailsTitleStyle from './css/CommunityDetailsTitle.module.css';
 import CommunityLocation from "./CommunityLocation";
 
-
 const CommunityDetailsTitle = () => {
-    const [community, setCommunitys] = useState([]);
+    const [community, setCommunity] = useState({}); 
+    // 더 이상 배열이 아닌 객체로 상태 설정
+    const [locationNum, setLocationNum] = useState(null);
+    // 초기값을 null로 설정 
 
-    const getCommunitys = () => {
+    const getCommunity = () => {
         fetch(`http://localhost:8001/communitys/2`)
         .then((response) => response.json())
         .then((data) => {
-            setCommunitys(data);
+            setCommunity(data);
+            setLocationNum(data.locationNum); 
+            // locationNum 값 설정
         });
     };
 
     useEffect(() => {
-        getCommunitys();
+        getCommunity();
     },[]);
 
     return(
@@ -26,14 +30,14 @@ const CommunityDetailsTitle = () => {
                         <div className={DetailsTitleStyle.communityTitleName}>{community.communityTitle}</div>
                         <div className={DetailsTitleStyle.keyword}></div>
                         <div className={DetailsTitleStyle.communityLocal}>
-                            <CommunityLocation localCode={community.locationNum}/>
+                            {locationNum !== null && <CommunityLocation localCode={locationNum}/>}
+                            {/* localNum의 값이 null이 아닐 경우에만 렌더링 되게 설정 */}
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-
+    );
 }
 
 export default CommunityDetailsTitle;
