c6823de349e3c307cc5b7aa46d4ac56ad1642285
afdsj <thdla3441@gmail.com>
2023-09-16 15:03:45 +0900

    NoticeList 수정

--- toegeungil/src/public/pages/NoticeList.js
+++ toegeungil/src/public/pages/NoticeList.js
@@ -1,24 +1,49 @@
-import { useEffect, useState } from "react";
-import NoticeTable from "../components/NoticeTable";
-
+import React, { useState, useEffect } from "react";
+import '../components/NoticeTable.css';
+import { Link } from "react-router-dom";
 
 const NoticeList = () => {
-    const [notice, setNotice] = useState([]);
+    const [list, setList] = useState([]);
+
+    const getList = () => {
+        fetch("http://localhost:8001/notices")
+            .then(response => response.json())
+            .then(data => setList(data))
+    }
 
     useEffect(
         () => {
-            fetch("http://localhost:8001/notices")
-                .then(response => response.json())
-                .then(data => setNotice(data))
-        }, [])
+            getList();
+        }
+    )
 
     return (
-        <>
-            <div>
-                <NoticeTable noticeHeadersName={['번호', '제목', '작성일']} children={notice}/>
-            </div>
-        </>
+        <div>
+            <table className="notice-wrapper">
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성일</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        list.map((notice) => (
+                            <tr className="notice-box" key={notice.noticeNum}>
+                                <Link to={`/notice/${notice.noticeNum}`}>
+                                    <td className="notice-number">{notice.noticeNum}</td>
+                                    <td className="notice-number">{notice.noticeContent}</td>
+                                    <td className="notice-number">{notice.noticeDate}</td>
+                                </Link>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div >
     )
 }
 
 export default NoticeList;
+
