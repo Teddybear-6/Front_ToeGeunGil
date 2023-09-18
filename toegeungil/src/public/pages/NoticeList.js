0ef5edc26df80f757f1fcb95929de48497632c14
afdsj <thdla3441@gmail.com>
2023-09-18 11:14:44 +0900

    NoticeList Link 추가

--- toegeungil/src/public/pages/NoticeList.js
+++ toegeungil/src/public/pages/NoticeList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import '../components/NoticeMain.css';
+import { Link } from "react-router-dom";
 
 const NoticeList = () => {
     const [list, setList] = useState([]);
@@ -13,16 +14,11 @@ const NoticeList = () => {
     useEffect(
         () => {
             getList();
-        }
-    )
-
-    const goView = (noticeNum) => {
-        window.location.href = `/notice/${noticeNum}`;
-    }
+        }, [])
 
     return (
         <div>
-            <table className="notice-wrapper">
+            <table className="table-wrapper">
                 <thead>
                     <tr>
                         <th>번호</th>
@@ -33,15 +29,24 @@ const NoticeList = () => {
                 <tbody>
                     {
                         list.map((notice) => (
-                            <tr key={notice.noticeNum} onClick={() => goView(notice.noticeNum)}>
+                            <tr key={notice.noticeNum}>
                                 <td>{notice.noticeNum}</td>
-                                <td>{notice.noticeContent}</td>
+                                <td>
+                                    <Link to={`/notice/${notice.noticeNum}`}
+                                        style={{ textDecoration: "none", color: "#87746B"}} className="title-link">{notice.noticeTitle}</Link>
+                                </td>
                                 <td>{notice.noticeDate}</td>
                             </tr>
                         ))
                     }
                 </tbody>
             </table>
+            {/* 관리자일 경우 */}
+            {/* <div className="main-button-box" >
+                <Link to="/notice">
+                    <button className="main-button">게시글 작성 버튼</button>
+                </Link>
+            </div> */}
         </div >
     )
 }
