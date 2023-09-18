4db721e6a85f12f70a7c0d51ecdbec5e3810b3b6
afdsj <thdla3441@gmail.com>
2023-09-18 20:56:30 +0900

    NoticeWrite_작성 페이지

--- toegeungil/src/public/pages/NoticeWrite.js
+++ toegeungil/src/public/pages/NoticeWrite.js
@@ -1,62 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../components/NoticeWrite.css";
 
 const NoticeWrite = () => {
-    const { noticeNum } = useParams();
-    const [title, setTitle] = useState(''); // 공지 제목
-    const [content, setContent] = useState(''); // 공지 내용
+    const [noticeTitle, setNoticeTitle] = useState(''); // 공지 제목
+    const [noticeContent, setNoticeContent] = useState(''); // 공지 내용
 
     const handleTitleChange = (e) => {
-        setTitle(e.target.value);
+        setNoticeTitle(e.target.value);
     }
-    console.log(title);
+    console.log(noticeTitle);
 
     const handleContentChange = (e) => {
-        setContent(e.target.value);
+        setNoticeContent(e.target.value);
+    }
+    console.log(noticeContent);
+
+    const cancelClick = () => {
+        alert("공지사항 작성이 취소 되었습니다");
     }
 
     const writeClick = () => {
-        alert("공지사항이 등록 되었습니다");
+        fetch(`http://localhost:8001/notices`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify({
+                    "noticeTitle": noticeTitle,
+                    "noticeContent": noticeContent,
+                }),
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert("공지사항이 등록 되었습니다");
+                    console.log("너야너")
+
+                } else {
+                    alert("공지사항 등록에 실패했습니다");
+                }
+            })
+            .catch(error => {
+                console.error("공지사항 등록 중 오류 발생 : ", error);
+                alert("공지사항 등록 중 오류 발생했습니다");
+            })
     }
-
-    const cancelClick = () => {
-        alert("공지사항이 등록이 취소 되었습니다");
-    }
-
-    console.log(content);
-
 
     return (
-        <div>
+        <div className="wrapper" >
             <h1 className="write-header">공지사항 작성</h1>
             <div className="write-wrapper">
                 <div className="write-col1">
                     <label className="write-title">공지 제목</label>
                     <input className="write-text1"
                         type="text"
-                        // value={noticeTitle}
+                        value={noticeTitle}
                         onChange={handleTitleChange}
                     />
                 </div>
                 <div className="write-col2">
                     <label className="write-content">공지 내용</label>
                     <textarea className="write-text2"
-                        // value={noticeContent}
+                        value={noticeContent}
                         onChange={handleContentChange}
                     />
+                    <div className="button">
+                        <Link to="/notice">
+                            <button className="cancel-button" onClick={cancelClick}>취소</button>
+                        </Link>
+                        <Link to="/notice">
+                            <button className="write-button" onClick={writeClick}>등록</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-            <Link to="/notice">
-                <button className="write-button" onClick={writeClick}>등록</button>
-            </Link>
-            <Link to="/notice">
-                <button className="cancel-button" onClick={cancelClick}>취소</button>
-            </Link>
         </div>
     )
 }
-
-
 
 export default NoticeWrite;
