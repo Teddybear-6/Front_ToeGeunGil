5445cdee7d7c0ca5dab937b91298bfec929395fc
afdsj <thdla3441@gmail.com>
2023-09-18 20:57:03 +0900

    NoticeView_상세 페이지

--- toegeungil/src/public/pages/NoticeView.js
+++ toegeungil/src/public/pages/NoticeView.js
@@ -1,44 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import '../components/NoticeView.css';
 
 const NoticeView = () => {
     const { noticeNum } = useParams();
     const [detail, setDetail] = useState({});
+    const [loading, setLoading] = useState(true); // 로딩 상태 관리
 
-    useEffect(
-        () => {
+    useEffect(() => {
+            setLoading(true);
             fetch(`http://localhost:8001/notices/${noticeNum}`)
                 .then(response => response.json())
-                .then(data => setDetail(data))
-        }, [])
+                .then(data => {
+                    setDetail(data);
+                    setLoading(false);
+                })
+        }, [noticeNum])
 
     return (
         <div className="view-wrapper">
             {
-                detail ? (
-                    <>
-                        <div className="view-name">
-                            <label>{detail.noticeTitle}</label>
-                        </div>
-                        <div className="view-content">
-                            <div className="view-date">
-                                <label>{detail.noticeDate}</label>
+                loading ? (
+                    "로딩 중"
+                ) : (
+                    detail ? (
+                        <>
+                            <div className="view-name">
+                                <label>{detail.noticeTitle}</label>
                             </div>
-                            <div className="view-text-box">
-                                <div className="view-text">
-                                    <label>{detail.noticeContent}</label>
+                            <div className="view-content">
+                                <div className="view-date">
+                                    <label>{detail.noticeDate}</label>
+                                </div>
+                                <div className="view-text-box">
+                                    <div className="view-text">
+                                        <label>{detail.noticeContent}</label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="view-button-box" >
-                            <Link to="/notice">
-                                <button className="view-button">목록으로</button>
-                            </Link>
-                        </div>
-                    </>
-                ) : "공지사항이 없습니다"
+                            <div className="view-button-box" >
+                                <Link to="/notice">
+                                    <button className="view-button">목록으로</button>
+                                </Link>
+                            </div>
+                        </>
+                    ) : "공지사항이 없습니다"
+                )
             }
         </div>
     )
