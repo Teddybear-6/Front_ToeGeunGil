a5b25b5f23b32076e960a398d751afb238edc8d4
dleksql1227 <dleksql1227@gmail.com>
2023-09-19 17:44:47 +0900

    Comment

--- toegeungil/src/public/community/components/CommunityDetailsComments.js
+++ toegeungil/src/public/community/components/CommunityDetailsComments.js
@@ -32,20 +32,36 @@ const CommunityDetailsComments = () => {
 
     useEffect(() => {
         getComments();
-    }, [communityNum]);
+    },[communityNum]);
 
     const handleComment = () => {
-        const newComment = {
+
+        const newCommentObject = {
             userNum: userNum,
             commentDetail: newComment,
-            commentWriteDate: new Date()
+            commentWriteDate: new Date().toISOString()
         };
 
-        setComments([comments, newComment]);
-        setNewComment(""); // 입력 필드 초기화
-        commentInputRef.current.value = "";
+        fetch(`http://localhost:8001/communitys/comments/${communityNum}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newCommentObject),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setNewComment(""); 
+                const commentWithTime = { ...data, commentWriteDate: new Date().toLocaleString()};
+                // 서버에서 반환된 새 댓글 데이터에 시간 추가 
+                setComments([...comments, commentWithTime]);
+                // 새로운 댓글을 기존 댓글 목록에 추가  
+            })
+            .catch((error) => {
+                console.error('댓글 등록에 실패하였습니다.', error);
+            })
+            window.location.reload();
     };
-
 
     return (
         <>
@@ -68,6 +84,18 @@ const CommunityDetailsComments = () => {
                             </div>
                         </div>
                     ))}
+            </div>
+            <div className={CommentsStyle.NewCommentBox}>
+                <input
+                    ref={commentInputRef}
+                    className={CommentsStyle.NewCommentInputBox}
+                    placeholder="댓글을 입력하세요"
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                />
+                <button className={CommentsStyle.NewCommentButton} onClick={handleComment}>
+                    등록
+                </button>
             </div>
         </>
     )
