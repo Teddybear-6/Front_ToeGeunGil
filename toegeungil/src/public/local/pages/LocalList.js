1fcbcd59903dd12f873049fe3c276fdd85dffcbc
afdsj <thdla3441@gmail.com>
2023-09-22 15:30:48 +0900

    버튼 링크 수정

--- toegeungil/src/public/local/pages/LocalList.js
+++ toegeungil/src/public/local/pages/LocalList.js
@@ -31,7 +31,7 @@ function LocalList() {
                 .then(response => {
                     if (response.ok) {
                         alert("지역 삭제되었습니다")
-                        // navigate("/service/local")
+                        // 메인 화면으로 전환
                         window.location.href = "/service/local";
                     } else {
                         throw new Error("지역 삭제 실패하였습니다")
@@ -66,7 +66,9 @@ function LocalList() {
                                             <td>{local.localName}</td>
                                             <td>
                                                 <button onClick={() => deleteLocal(local.localCode)}>삭제</button>
-                                                <button>수정</button>
+                                                <Link to={`/service/local/${localCode}/modify`}>
+                                                    <button>수정</button>
+                                                </Link>
                                             </td>
                                         </tr>
                                     ))}
