ecd0f4a418481c7f47fa0d3ce6fae57e2162979b
afdsj <thdla3441@gmail.com>
2023-09-23 18:52:50 +0900

    CategoryList 삭제 버튼 수정

--- toegeungil/src/public/category/components/CategoryList.js
+++ toegeungil/src/public/category/components/CategoryList.js
@@ -23,6 +23,25 @@ function CategoryList() {
         alert("카테고리를 작성하시겠습니까?")
     }
 
+    const deleteCategory = (categoryCode) => {
+        if (window.confirm("정말로 삭제하시겠습니까?")) {
+            fetch(process.env.REACT_APP_URL + `/category/${categoryCode}`, { method: "DELETE" })
+                .then(response => {
+                    if (response.ok) {
+                        alert("카테고리가 삭제되었습니다")
+                        // 메인 화면으로 전환
+                        window.location.href = "/service/category";
+                    } else {
+                        throw new Error("카테고리 삭제 실패하였습니다")
+                    }
+                })
+                .catch(error => {
+                    console.error("카테고리 삭제 중 오류 발생 : ", error);
+                    alert("카테고리 삭제 중 오류가 발생하였습니다");
+                })
+        }
+    }
+
     return (
         <div className='layout'>
             {
@@ -44,7 +63,7 @@ function CategoryList() {
                                             <td>{category.categoryCode}</td>
                                             <td>{category.categoryName}</td>
                                             <td>
-                                                <button >삭제</button>
+                                                <button onClick={() => deleteCategory(category.categoryCode)}>삭제</button>
                                                 <Link to={`/service/category/${category.categoryCode}/modify`}>
                                                     <button>수정</button>
                                                 </Link>
