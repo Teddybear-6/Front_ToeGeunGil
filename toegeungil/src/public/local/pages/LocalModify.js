e8f6600241affd7a8c841be9a1d90e770a496f96
afdsj <thdla3441@gmail.com>
2023-09-23 16:53:47 +0900

    관리자 권한 생성

--- toegeungil/src/public/local/pages/LocalModify.js
+++ toegeungil/src/public/local/pages/LocalModify.js
@@ -13,7 +13,7 @@ function LocalModify() {
         if (sessionStorage.getItem("Authorizaton")) {
             setUser(jwt_decode(sessionStorage.getItem("Authorizaton")))
         }
-        
+
         fetch(process.env.REACT_APP_URL + `/local/${localCode}`)
             .then(response => response.json())
             .then(data => setLocalName(data.localName))
@@ -29,54 +29,60 @@ function LocalModify() {
 
     const updateClick = () => {
         // if (user && user.auth[0] === 'ADMIN'){
-            fetch(process.env.REACT_APP_URL + `/local/${localCode}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json; charset=UTF-8"
-                },
-                body: JSON.stringify({
-                    "localName": localName,
-                }),
+        fetch(process.env.REACT_APP_URL + `/local/${localCode}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({
+                "localName": localName,
+            }),
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert("지역이 수정되었습니다");
+                    navigate("/service/local");
+                } else {
+                    throw new Error("지역 수정에 실패하였습니다");
+                }
             })
-                .then(response => {
-                    if (response.ok) {
-                        alert("지역이 수정되었습니다");
-                        navigate("/service/local");
-                    } else {
-                        throw new Error("지역 수정에 실패하였습니다");
-                    }
-                })
-                .catch(error => {
-                    console.error("지역 수정 중 오류 발생 :", error);
-                    alert("지역 수정 중 오류가 발생하였습니다");
-                })
+            .catch(error => {
+                console.error("지역 수정 중 오류 발생 :", error);
+                alert("지역 수정 중 오류가 발생하였습니다");
+            })
         // }else{
         //     alert("관리자가 아닙니다 공지사항 작성 권한이 없습니다");
         // }
-        
+
     }
 
     return (
         <div className='layout'>
             <div className="local-wrapper">
-            <h1 className="local-title">지역 수정</h1>
-                <div className="local-wrapper2">
-                    <label className="local-name">수정하는 지역명</label>
-                    <input className="local-text"
-                        type="text"
-                        value={localName}
-                        onChange={nameChange}
-                    />
-                </div>
-                <div className="local-button">
-                    <Link to="/service/local">
-                        <button className="local-cancel" onClick={cancelClick}>취소</button>
-                    </Link>
-                    <Link to='/service/local'>
-                        <button className="local-regist" onClick={updateClick}>등록</button>
-                    </Link>
-                </div>
-                </div>
+                <h1 className="local-title">지역 수정</h1>
+                {user && user.auth[0] === 'ADMIN' ? (
+                    <div className="local-wrapper2">
+                        <label className="local-name">수정하는 지역명</label>
+                        <input className="local-text"
+                            type="text"
+                            value={localName}
+                            onChange={nameChange}
+                        />
+                    </div>
+                ) : (
+                    <p>관리자가 아닙니다 공지사항 수정 권한이 없습니다</p>
+                )}
+                {!user ? null : (user.auth[0] == 'ADMIN') ?
+                    <div className="local-button">
+                        <Link to="/service/local">
+                            <button className="local-cancel" onClick={cancelClick}>취소</button>
+                        </Link>
+                        <Link to='/service/local'>
+                            <button className="local-regist" onClick={updateClick}>등록</button>
+                        </Link>
+                    </div>
+                    : null}
+            </div>
         </div>
     )
 }
