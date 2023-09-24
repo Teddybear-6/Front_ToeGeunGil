d7e93ed715afc530e495a60493d093eb5c0035b7
bearnyong <rhalsdud216@gmail.com>
2023-09-24 11:28:42 +0900

    social_모임 참여 오류 수정

--- toegeungil/src/public/social/components/componentAPI/SocialParticipate.js
+++ toegeungil/src/public/social/components/componentAPI/SocialParticipate.js
@@ -3,51 +3,48 @@ import DetailsStyle from '../css/SocialDetails.module.css';
 import jwt_decode from "jwt-decode";
 
 
-function SocialParticipate({socialNum, userNum}) { //소설 번호와 회원 번호 받아오기
+function SocialParticipate({socialNum}) { //소설 번호 받아오기
+
+    console.log("받아온 소셜 넘버 : ", socialNum); //받아온 소셜 넘버 확인
 
     const [user, setUser] = useState();
+    const [participate, setParticipate] = useState();
 
-    const [participate, setParticipate] = useState({
-        socialNum: '', //소셜 번호
-        userNum: '', //회원 번호
-    });
-
-    const participateHandler = () => {
-
+    useEffect(() => {
         //권한설정
         if (sessionStorage.getItem("Authorizaton")) {
             setUser(jwt_decode(sessionStorage.getItem("Authorizaton")))
         }
+    }, []);
 
-        if (!user) {
+    const participateHandler = () => {
+
+        console.log("나 유저: ", user)
+
+        if ((user === undefined) && (user === null)) {
             // 유저가 아닐 경우
             alert("회원만 참여가능합니다.")
         } else if (!participate) {
             // 참여 신청이 되어있지 않을 경우
-            if (window.confirm("참여하시겠습니까")) {
-                fetch(process.env.REACT_APP_URL + `/socials/participate/${socialNum}`, {
+            if (window.confirm("[social] 참여하시겠습니까")) {
+                fetch(process.env.REACT_APP_URL + `/socials/participate/${socialNum}/${user.no}`, {
                     method: "POST",
                 })
                 .then(res => res.text())
                 .then(res => {
-                    alert(res)
-                    if (res == "참가 완료되었습니다.") {
-                        setParticipate(true)
-                    }
+                    alert(res) //참가 완료 알림
+                    setParticipate(true)
                 }).catch(r => console.log(r))
             }
         } else if (participate) {
             if (window.confirm("참여 취소하시겠습니까")) {
-                fetch(process.env.REACT_APP_URL + `/socials/participate/${socialNum}`, {
+                fetch(process.env.REACT_APP_URL + `/socials/participate/${socialNum}/${user.no}`, {
                     method: "POST",
                 })
                 .then(res => res.text())
                 .then(res => {
-                    alert(res)
-                    if (res == "참가 취소 되었습니다.") {
-                        setParticipate(false)
-                    }
-                    
+                    alert(res) //참가 취소 알림
+                    setParticipate(false)
                 }).catch(r => console.log(r))
             }
         }
@@ -55,7 +52,7 @@ function SocialParticipate({socialNum, userNum}) { //소설 번호와 회원 번
 
     return (
         <>
-            <button className={DetailsStyle.buttonStyle} onClick={participateHandler} name="userNum">참여하기</button>
+            <button className={DetailsStyle.buttonStyle} onClick={participateHandler}>참여하기</button>
             {/* <button className={DetailsStyle.buttonStyle}>취소하긴</button> */}
         </>
     )
