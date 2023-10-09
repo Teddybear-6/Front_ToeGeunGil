4a34c6ee2bb83bcf04bbee8d1d5be727285fd24c
JIHWAN <jjjh0508@gmail.com>
2023-10-09 15:30:37 +0900

    키워드 호버시 숨겨진 키워드 보이게 수정

--- toegeungil/src/public/hobby/components/hobbyCardKeyword.js
+++ toegeungil/src/public/hobby/components/hobbyCardKeyword.js
@@ -1,30 +1,52 @@
 
 import HobbyKeyword from './HobbyKeyword';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import CardStyle from './hobbyCard.module.css';
 function HobbyCardkeyword({ keyword }) {
   useEffect(() => {
 
   }, [keyword])
-
+  const newArr = [];
+  const newArrhide = [];
   const keywordArr = (keyword) => {
-
-    const newArr = [];
     for (let index = 0; index < keyword?.length; index++) {
       if (index < 2) {
         newArr.push(<div key={index} className={CardStyle.keywordCard}>
           <HobbyKeyword keyword={keyword[index]} />
         </div>)
       }
-
-
     }
     return newArr;
   }
+
+
+  const keywordArrhide = (keyword) => {
+    if (newArr?.length < keyword?.length) {
+      for (let index = newArr?.length; index < keyword?.length; index++) {
+
+        newArrhide.push(<div key={index} className={CardStyle.keywordCardhide}>
+          <HobbyKeyword keyword={keyword[index]} />
+        </div>)
+
+      }
+      return newArrhide;
+    }
+
+  }
+
+
   return (
     <>
-      {keywordArr(keyword)}
 
+      {keywordArr(keyword)}
+      {keyword?.length >= 3 &&
+        <>
+          <div className={CardStyle.keywordUnName}>
+            <p className={CardStyle.keywordName}>···</p>
+          </div>
+          {keywordArrhide(keyword)}
+        </>
+      }
 
 
     </>
