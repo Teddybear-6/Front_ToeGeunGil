8ae154fb81c2e9fa330ab8c50204fbb787194daf
bearnyong <rhalsdud216@gmail.com>
2023-10-13 21:07:18 +0900

    user css

--- toegeungil/src/public/user/findpassword/Findpass.js
+++ toegeungil/src/public/user/findpassword/Findpass.js
@@ -2,22 +2,12 @@
 import React, { useState } from "react";
 import { Link, json, useNavigate } from 'react-router-dom';
 import './Findpass.css';
-
-import name_icon from '../imgs/name.png';
-import nickname_icon from '../imgs/nick.png';
-import id_icon from '../imgs/id.png';
-import email_icon from '../imgs/001.png';
+import email_icon from '../imgs/email.png';
 
 const Findpass = () => {
 
     const [action, setAction] = useState("Findpass");
     const [email , setEmail]= useState();
-    // const [login, setFindpass] = useState({
-    //     Name: "",
-    //     nickName: "",
-    //     Email: ""
-    // });
-
     const navigate = useNavigate();
 
     const onClickHandler = () => {
