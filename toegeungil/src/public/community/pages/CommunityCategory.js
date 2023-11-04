21416b56ebf1624cb89e05986bf445e69ccff3c4
dleksql1227 <dleksql1227@gmail.com>
2023-11-04 19:11:10 +0900

    CommunityCategory추가

---
+++ toegeungil/src/public/community/pages/CommunityCategory.js
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Paging from "../components/Paging";
+import CommunityCategory from "../components/CommunityCategory";
+
+function CommunityCategory({ localfilters }) {
+    const [community, setCommunity] = useState([]);
+    const [page, setPages] = useState(1);
+    const [pageCount, setPageCount] = useState();
+    const categoryCode = useLocation().state;
+
+
+    useEffect(() => {
+        if (localfilters === "0" || localfilters === undefined || localfilters === null) {
+            fetch(process.env.REACT_APP_URL + `/communitys/category/${categoryCode}?page=${page - 1}&size=12`)
+                .then((response) => response.json())
+                .then((data) => setCommunity(data));
+
+            fetch(process.env.REACT_APP_URL + `/communitys/category/${categoryCode}/size`)
+                .then((response) => response.json())
+                .then((response) => setPageCount(response));
+        } else {
+            fetch(process.env.REACT_APP_URL + `/communitys/category/${categoryCode}/local/${localfilters}?page=${page - 1}&size=12`)
+                .then((response) => response.json())
+                .then((response) => setCommunity(data));
+
+            fetch(process.env.REACT_APP_URL + `/communitys/category/${categoryCode}/local/${localfilters}/size`)
+                .then((response) => response.json())
+                .then((response) => setPageCount(response));
+        }
+    }, [categoryCode, page, localfilters]);
+
+    const setPage = useCallback(
+        (page) => {
+            setPage(page);
+        }
+    );
+
+    return(
+        <>
+        <Paging count={pageCount} setPage={setPage} page={page} categoryCode={categoryCode}/>
+        </>
+    );
+}
+
+export default CommunityCategory;
