c74089c0256abe3f3073b4a2179eb41def89ad06
dleksql1227 <dleksql1227@gmail.com>
2023-11-05 18:58:24 +0900

    paging 및 filter추가

--- toegeungil/src/public/community/pages/CommunityMain.js
+++ toegeungil/src/public/community/pages/CommunityMain.js
@@ -1,14 +1,46 @@
 import CommunityList from "../components/CommunityList";
 import "../../layout/layout.css";
+import { useCallback, useEffect, useState } from "react";
+import Paging from "../components/Paging";
 
 
+function CommunityMain({localfilters}) {
+    const [community, setCommunity] = useState([]);
+    const [page, setPages] = useState(1);
+    const [pageCount, setPageCount] = useState();
 
-function CommunityMain({ }) {
+    useEffect(() => {
+        if(localfilters === "0" || localfilters === undefined || localfilters === null){
+            fetch(process.env.REACT_APP_URL +  `/communitys?page=${page - 1}&size=12`)
+            .then((response) => response.json())
+            .then((data) => setCommunity(data))
+
+            fetch(process.env.REACT_APP_URL + `/communitys/size`)
+            .then((response) => response.json())
+            .then((response) => setPageCount(response))
+        } else{
+            fetch(process.env.REACT_APP_URL + `/communitys/local/${localfilters}?page=${page - 1}&size=12`)
+            .then((response) =>  response.json())
+            .then((data) => setCommunity(data))
+
+            fetch(process.env.REACT_APP_URL + `/communitys/local/${localfilters}/size`)
+            .then((response) => response.json())
+            .then((response) => setPageCount(response))
+        }
+    }, [page], localfilters);
+
+    const setPage = useCallback(
+        (page) => {
+            setPages(page)
+        }
+    )
+
     return (
         <>
             <div className='toegeungillayout'>
                 <CommunityList />
             </div>
+            <Paging count={pageCount} setPage={setPage} page={page} localfilters={localfilters}/>
         </>
     );
 }
