ec30fab38ce8fd7f95cbc3adb25d32a0c1eeb9d0
dleksql1227 <dleksql1227@gmail.com>
2023-11-09 20:25:20 +0900

    CommunitySearch 추가

--- toegeungil/src/public/community/pages/CommunitySearch.js
+++ toegeungil/src/public/community/pages/CommunitySearch.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import Paging from "../components/Paging";
+import CommunityList from '../components/CommunityList';
 
 function CommunitySearch(){
     const [community, setCommunity] = useState([]);
@@ -17,11 +18,11 @@ function CommunitySearch(){
             }else{
                 return response.json();
             }
-        }).then(res => setCommunity(res))
+        }).then((response) => setCommunity(response))
 
         fetch(process.env.REACT_APP_URL + `/communitys/search/size?communityTitle=${communityTitle}`)
-        .then(res => res.json())
-        .then(res => setPageCount(res))
+        .then(response => response.json())
+        .then(response => setPageCount(response))
     }, [page, communityTitle]);
 
     const setPage = useCallback((page) => {
@@ -30,8 +31,17 @@ function CommunitySearch(){
 
     return(
         <>
-            
+                <div className="menuFont">Community - '{communityTitle}' 전체 결과</div>
+                <hr className='hrSty marB50'/>
+                {
+                    !community ? <div>검색결과가 없습니다.</div> : (<CommunityList community={community}/>)
+                }
+                {
+                    !community ? null : <Paging count={pageCount} setPage={setPage} page={page}/>
+                }
         </>
     )
 
 }
+
+export default CommunitySearch;
